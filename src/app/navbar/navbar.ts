8d09ca00c38cd2e22eb95ec38f113fe077c3dae4
gunapriya325 <gunapriya325@gmail.com>
2026-07-13 21:50:08 +0530

    menu button change

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,4 +8,29 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  closeMenu() {
+    const menu = document.getElementById('menu-toggle') as HTMLInputElement;
+
+    if (menu) {
+      menu.checked = false;
+    }
+  }
+
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+
+    const menu = document.getElementById('menu-toggle') as HTMLInputElement;
+
+    if (!menu?.checked) return;
+
+    const clickedInsideNavbar =
+      this.elementRef.nativeElement.contains(event.target);
+
+    if (!clickedInsideNavbar) {
+      menu.checked = false;
+    }
+  }
+}
